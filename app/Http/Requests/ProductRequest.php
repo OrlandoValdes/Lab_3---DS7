77253c3e8a14bf697d8758d9151b0dfeca1bc61b
OrlandoValdes <orlando.valdes2@utp.ac.pa>
2026-04-25 16:02:49 -0500

    Laravel-Crud Realizado

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'description' => 'required|max:255',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::resource('products', ProductController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
